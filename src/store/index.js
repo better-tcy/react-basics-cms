ab0ee300469bf9b84ab6befd906f55afad227f32
TuoCangYu <2367314268@qq.com>
2022-01-21 15:28:30 +0800

    增加redux-devtools工具

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,9 @@ import {
 
 import thunkMiddleware from 'redux-thunk'
 
+// 安装redux-devtools-extension的可视化工具。
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 import reducer from './reducer'
 
 
@@ -12,7 +15,7 @@ import reducer from './reducer'
 const storeenhancer = applyMiddleware(thunkMiddleware)
 
 // 创建store
-const store = createStore(reducer, storeenhancer)
+const store = createStore(reducer, composeWithDevTools(storeenhancer))
 // thunkMiddleware这个中间件 已经被应用到store里面了 
 // dispatch可以派发一个函数了 在这个函数里面可以进行网络请求 然后把返回的数据 放在action里面 然后再次进行dispatch
 // 这样网络请求就不是依赖组件了 而是在store中完成了
