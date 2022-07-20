af67e36c41e3700fc24abe0a5dc7da2fa08995f1
tuocangyu <1054356997@qq.com>
2022-07-20 22:36:15 +0800

    feat: 优化首页文本过多问题

--- src/view/content/home/Home.js
+++ src/view/content/home/Home.js
@@ -42,11 +42,14 @@ const Home = () => {
   // 是否显示生成后的页面
   const [isShowPage, setIsShowPage] = useState(false)
 
+  const timer = useRef()
+
   const textDom = useRef(null)
 
   // 所有文本内容
   const allText = useRef(
-    `你可以编写一个这样的js配置来生成一个页面（大概需要花费三分钟左右 请耐心看完）
+    `你可以编写一个这样的js配置来生成一个页面（大概需要花费您一分钟的时间，请耐心看完）
+     全部配置请查看在线文档：https://betteryourself-tcy.github.io/json-page-markdown/， 也可从代码UsePage.js组件中查看
     const pageConfig = {
       // 接口的配置
       pageRequestUrl: {
@@ -93,8 +96,6 @@ const Home = () => {
             customizeOptionsValueKey: 'id',
             customizeOptionsLabelKey: 'name',
             url: '/firstSelectData/',
-            // requestKey: 'programmingLanguage',
-            // requestValue: 10,
             rules: []
           },
           {
@@ -124,268 +125,16 @@ const Home = () => {
       },
       // 表格区域配置
       pageTableConfig: {
-        // 默认都为true
-        // isShowAddBtn: false,
-        // isShowCheckDetailsBtn: false,
-        // isShowUpdateBtn: false,
-        // isShowRemoveBtn: false,
-        // isShowEnableDisableBtn: false,
-        // isShowActionColumns: false,
-        // 操作列的宽度 默认为500 如果表格列数较多，
-        // 请给每个列添加宽度（columns中的每一项增加width属性）这样表格会增加横向滚动条，操作列悬浮固定在右侧
-        // 如果isShowActionColumns为false又想出现横向滚动条的话 可以给columns中其他属性添加fixed属性（详见antd官网）
-        // actionColumnsWidth: 500,
-        // 表格是否可滚动，也可以指定滚动区域的宽、高（详见antd官网）
-        scroll: { y: 360 },
-        columns: [
-          {
-            title: '用户名称',
-            dataIndex: 'name',
-            key: 'name',
-            align: 'center'
-          },
-          {
-            title: '手机号',
-            dataIndex: 'iphone',
-            key: 'iphone',
-            align: 'center'
-          },
-          {
-            title: '职务',
-            dataIndex: 'jobTitle',
-            key: 'jobTitle',
-            align: 'center',
-            render: (text) => {
-              if (text === 1) {
-                return <Tag color="processing">管理员</Tag>
-              } else if (text === 2) {
-                return <Tag color="processing">测试员</Tag>
-              }
-            }
-          }
-        ],
-        // 如果按钮权限不具体到表格行信息的时候 下面函数可不传 默认按钮都显示
-        // 根据行数据 是否渲染 查看按钮
-        // accordingRowIsRenderCheckBtnFun(record) {
-        //   // 返回值必须为true && false
-
-        // },
-        // 根据行数据 是否渲染 修改按钮
-        // accordingRowIsRenderUpdateBtnFun(record) {
-
-        // },
-        // 根据行数据 是否渲染 删除按钮
-        accordingRowIsRenderRemoveBtnFun(record) {
-          if (record.id === 1) {
-            return false
-          } else {
-            return true
-          }
-        }
-        // 根据行数据 是否渲染 启用停用
-        // accordingRowIsRenderEDBtnFun(record) {
-
-        // }
+        // 可定制每一项
+        ...
       },
       // 弹窗配置
       pageModalConfig: {
-        // width: 500, // 弹窗宽度 默认为560
-        // labelCol:{}, // 弹窗中表单label标签布局 详见antd官网 默认值 { offset: 0, span: 6 }
-        // wrapperCol:{},// 弹窗表单中需要为输入控件设置布局样式时 使用该属性，用法同 labelCol 详见antd官网 默认值 { offset: 1, span: 12 }
-        // layout:'' // 表单布局 可选值 horizontal | vertical | inline, 默认值为horizontal
-        // maskClosable: false, // 点击蒙层是否允许关闭 默认为true
-        // okText:'完成' // 确认按钮文字 默认为确定
-        // cancelText:'关闭' // 取消按钮文字 默认为取消
-        modalItemArr: [
-          {
-            type: 'input',
-            label: '姓名',
-            field: 'name',
-            placeholder: '请输入姓名',
-            // disabled: true, // 是否禁用 默认为false
-            rules: [{ required: true, message: '请输入姓名' }]
-          },
-          {
-            type: 'input',
-            label: '手机号',
-            field: 'phone',
-            placeholder: '请输入手机号',
-            isNumber: true, // 是否是数字输入框 默认为false
-            rules: [{ required: true, message: '请输入手机号' }]
-          },
-          {
-            type: 'password',
-            label: '密码',
-            field: 'password',
-            placeholder: '请输入密码',
-            isNumber: true, // 是否是数字输入框 默认为false
-            rules: []
-          },
-          {
-            type: 'select',
-            label: '选择水果',
-            field: 'fruit',
-            placeholder: '请选择水果',
-            mode: '', // 设置 Select 的模式为多选或标签 可选值为 multiple | tags，不传为单选
-            customizeOptionsValueKey: 'id', // 自定义options中value字段  默认为value
-            customizeOptionsLabelKey: 'name', // 自定义options中label字段 默认为label
-            options: [
-              {
-                name: '苹果',
-                id: 1
-              },
-              {
-                name: '香蕉',
-                id: 2
-              }
-            ],
-            rules: []
-          },
-          {
-            type: 'radio',
-            label: '性别',
-            field: 'radioVal',
-            customizeOptionsValueKey: 'id', // 默认为value
-            customizeOptionsLabelKey: 'name', // 默认为label
-            options: [
-              {
-                id: 1,
-                name: '男'
-              },
-              {
-                id: 2,
-                name: '女'
-              }
-            ],
-            rules: []
-          },
-          {
-            type: 'checkbox',
-            label: '汽车',
-            field: 'checkboxVal',
-            customizeOptionsValueKey: 'id', // 默认为value
-            customizeOptionsLabelKey: 'name', // 默认为label
-            options: [
-              {
-                id: 1,
-                name: '红旗'
-              },
-              {
-                id: 2,
-                name: '吉利'
-              }
-            ],
-            rules: []
-          },
-          {
-            type: 'tree',
-            label: '学习的编程语言',
-            field: 'treeVal',
-            customizeOptionsValueKey: 'id', // 默认为value
-            customizeOptionsLabelKey: 'name', // 默认为label
-            customizeOptionsChildrenKey: 'children', // 默认为children
-            options: [
-              {
-                id: 1,
-                key: 1,
-                name: '编程语言',
-                children: [
-                  {
-                    id: 2,
-                    key: 2,
-                    name: 'JavaScript'
-                  }
-                ]
-              }
-            ],
-            rules: []
-          },
-          {
-            type: 'datePicker',
-            label: '选择日期',
-            field: 'datePickerVal',
-            format: '', // 默认为时间戳
-            rules: []
-          },
-          {
-            type: 'rangePicker',
-            label: '选择日期区间',
-            field: ['startDate', 'endDate'],
-            format: '', // 默认为时间戳
-            showTime: true,
-            rules: []
-          },
-          {
-            type: 'upload',
-            label: '上传',
-            field: 'uploadVal',
-            actionUrl: 'http://localhost:3001/upload', // 上传的地址
-            headers: {},
-            accept: '', // 接受上传的文件类型, 详见 https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file#accept
-            data: {}, // 上传所需额外参数或返回上传额外参数的方法
-            listType: '', //	上传列表的内建样式，支持三种基本样式 text, picture 和 picture-card
-            rules: []
-          },
-          {
-            type: 'textArea',
-            label: '文本域',
-            field: 'textAreaVal',
-            placeholder: '请输入内容',
-            rows: 3, // 文本域行数 默认为2
-            maxLength: 10, // 输入内容最大长度 默认为100
-            rules: []
-          },
-          {
-            type: 'cascader',
-            label: '级联选择',
-            field: 'cascaderVal',
-            placeholder: '请选择数据',
-            multiple: false, // 是否多选 默认为false
-            expandTrigger: 'hover', // 次级菜单的展开方式，可选 'click' 和 'hover' 默认为click
-            changeOnSelect: true, //（单选时生效）当此项为 true时，点选每级菜单选项值都会发生变化 默认为false
-            // customizeOptionsValueKey: 'id', // 默认为value
-            // customizeOptionsLabelKey: 'name', // 默认为label
-            // customizeOptionsChildrenKey:'child' // 默认为children
-            options: [
-              {
-                label: '前端',
-                value: 1,
-                children: [
-                  {
-                    label: 'Vue',
-                    value: 2
-                  },
-                  {
-                    label: 'React',
-                    value: 3
-                  }
-                ]
-              },
-              {
-                label: '后端',
-                value: 7,
-                children: [
-                  {
-                    label: 'Java',
-                    value: 8
-                  },
-                  {
-                    label: 'Golang',
-                    value: 9
-                  },
-                  {
-                    label: 'Python',
-                    value: 10
-                  }
-                ]
-              }
-            ],
-            rules: []
-          }
-        ]
+        // 可定制每一项
+        ...
       }
     }
-    然后就可以生成一个页面啦
+    然后就可以生成一个带有交互功能的页面啦
   `
   )
 
@@ -812,11 +561,10 @@ const Home = () => {
   // 渲染配置文字的函数
   const setTextFun = () => {
     let index = 0
-    const timer = setInterval(() => {
+    timer.current = setInterval(() => {
       const strItem = allText.current.substring(index, index + 1)
-      console.log(timer)
       if (!strItem) {
-        clearInterval(timer)
+        clearInterval(timer.current)
         setTimeout(() => {
           setIsShowPage(true)
         }, 500)
@@ -832,6 +580,9 @@ const Home = () => {
 
   useEffect(() => {
     setTextFun()
+    return () => {
+      clearInterval(timer.current)
+    }
   }, [])
 
   return (
